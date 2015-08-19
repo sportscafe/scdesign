cb16593e3771f163953f863f0e0d42eefb7e3b02
Sahil Ahuja <sahilahuja@gmail.com>
2015-08-19 11:21:10 +0000

    Making the design dev work

--- browsersync/design/js/scorer.js
+++ browsersync/design/js/scorer.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('rana', [
-        'ngCookies',
-        'ngResource',
-        'ngSanitize',
-        'ngRoute',
+        // 'ngCookies',
+        // 'ngResource',
+        // 'ngSanitize',
+        // 'ngRoute',
         'ngMaterial',
         'ngAnimate'
     ]);
